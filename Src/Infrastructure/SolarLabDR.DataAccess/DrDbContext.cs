1e33a47073694313126aad6c6b375a3c1444b21f
Kuznetsov Maksim <kuznecov_mv@akelon.com>
2025-08-11 21:31:51 +0300

    Добавил начальных данныз для БД и описание

--- Src/Infrastructure/SolarLabDR.DataAccess/DrDbContext.cs
+++ Src/Infrastructure/SolarLabDR.DataAccess/DrDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SolarLabDR.DataAccess.Configuration;
+using SolarLabDR.Domain;
 
 namespace SolarLabDR.DataAccess
 {
@@ -7,7 +8,7 @@ namespace SolarLabDR.DataAccess
     {
         public DrDbContext(DbContextOptions options) : base(options)
         {
-
+            Database.EnsureCreated();
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -15,6 +16,43 @@ namespace SolarLabDR.DataAccess
 
             modelBuilder.ApplyConfiguration(new ImageConfiguration());
             modelBuilder.ApplyConfiguration(new PersonConfiguration());
+
+            modelBuilder.Entity<Person>().HasData(
+                new Person 
+                { Id = Guid.Parse("01988eaa-649d-7ee3-b8ce-0291de8587be"),
+                     Name = "Валерий",
+                    LastName = "Славко",
+                    Date = DateTime.Parse("2001-08-15 00:00:00+00").ToUniversalTime(),
+                    Email = "slavko@mail.ru" 
+                },
+                new Person
+                {
+                    Id = Guid.Parse("019898fb-da44-7018-879c-b0826773613a"),
+                    Name = "Петр",
+                    LastName = "Корочаев",
+                    Date = DateTime.Parse("1981-08-10 00:00:00+00").ToUniversalTime(),
+                    Email = "testhendler@mail.ru"
+                },
+                new Person
+                {
+                    Id = Guid.Parse("0198990c-b2ff-711e-b008-135e3cdae65d"),
+                    Name = "Ася",
+                    LastName = "Бякина",
+                    Date = DateTime.Parse("2003-08-17 00:00:00+00").ToUniversalTime(),
+                    Email = "asin@mail.ru"
+                },
+                new Person
+                {
+                    Id = Guid.Parse("0198888d-6478-7548-848b-d5726a2dccc4"),
+                    Name = "Вася",
+                    LastName = "Букин",
+                    Date = DateTime.Parse("2005-08-20 00:00:00+00").ToUniversalTime(),
+                    Email = "bukin@mail.ru"
+                });
+
+            //modelBuilder.Entity<Image>().HasData(
+
+
         }
     }
 }
